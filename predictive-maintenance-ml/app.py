631a830b5164c732a8815a2e3d19be9fe7c7c558
JayMakario <135024670+JayMakario@users.noreply.github.com>
2026-03-15 03:10:30 +0000

    Update app.py

--- predictive-maintenance-ml/app.py
+++ predictive-maintenance-ml/app.py
@@ -6,7 +6,7 @@ import joblib
 BASE_DIR = Path(__file__).resolve().parent
 MODEL_PATH = BASE_DIR / "best_model.pkl"
 
-model = joblib.load("MODEL_PATH")
+model = joblib.load(MODEL_PATH)
 
 st.title("Predictive Maintenance Model")
 st.write("Predict whether industrial equipment is at risk of failure.")
@@ -40,3 +40,5 @@ if st.button("Predict Failure"):
     st.subheader("Prediction Result")
     st.write("Failure Prediction:", int(prediction))
     st.write("Failure Probability:", round(float(probability), 4))
+
+
